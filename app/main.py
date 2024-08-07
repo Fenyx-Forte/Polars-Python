f694801a0e3fa63e4400ad830e274ea515ac6c38
Fenyx Forte <fenyx.studies@gmail.com>
2024-08-07 09:45:03 -0300

    corrigindo remocao de acentos e caracteres especiais no etl

--- app/main.py
+++ app/main.py
@@ -4,6 +4,6 @@ from uteis import my_time
 if __name__ == "__main__":
     report_time = my_time.now_brasilia_str_report()
 
-    # aplicacao.main()
+    aplicacao.main()
     aplicacao.create_enade_report(report_time)
     # aplicacao.testando_streamlit()
